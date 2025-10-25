de15bfa2c9046c5c6b06fc928ccff81a1d618e87
James Green <gre23005@byui.edu>
2025-10-25 16:15:28 -0400

    your commit message here

--- blog/blog.js
+++ blog/blog.js
@@ -1,40 +1,38 @@
 const articles = [
-{
-id: 1,
-title: "Septimus Heap Book One: Magyk",
-date: "July 5, 2022",
-description:
-"If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.",
-imgSrc: "https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg",
-imgAlt: "Book cover for Septimus Heap 1",
-ages: "10-14",
-genre: "Fantasy",
-stars: "****"
-},
-{
-id: 2,
-title: "Magnus Chase Book One: Sword of Summer",
-date: "December 12, 2021",
-description:
-"The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.",
-imgSrc:
-"https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300",
-imgAlt: "Book cover for Magnus Chase 1",
-ages: "12-16",
-genre: "Fantasy",
-stars: "⭐⭐⭐⭐"
-},
-{
-id: 3,
-title: "Belgariad Book One: Pawn of Prophecy",
-date: "Feb 12, 2022",
-description:
-"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
-imgSrc:
-"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
-imgAlt: "Book cover for Pawn of Prophecy",
-ages: "12-16",
-genre: "Fantasy",
-stars: "⭐⭐⭐⭐⭐"
-}
+  {
+    id: 1,
+    title: "The Way of Kings",
+    author: "Brandon Sanderson",
+    date: "October 10, 2025",
+    description: "A breathtaking epic of war, honor, and destiny. The first entry in the Stormlight Archive blends rich worldbuilding with deep character growth.",
+    imgSrc: "images/way_of_kings.jpg",
+    imgAlt: "The Way of Kings book cover",
+    ages: "16+",
+    genre: "Fantasy",
+    stars: "⭐⭐⭐⭐⭐"
+  },
+  {
+    id: 2,
+    title: "Six of Crows",
+    author: "Leigh Bardugo",
+    date: "October 11, 2025",
+    description: "A dark fantasy heist featuring a band of misfits trying to pull off the impossible. Gritty, fast-paced, and full of twists.",
+    imgSrc: "images/six_of_crows.jpg",
+    imgAlt: "Six of Crows book cover",
+    ages: "14+",
+    genre: "Fantasy",
+    stars: "⭐⭐⭐⭐"
+  },
+  {
+    id: 3,
+    title: "Belgariad Book One: Pawn of Prophecy",
+    author: "David Eddings",
+    date: "Feb 12, 2022",
+    description: "A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+    imgSrc: "https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+    imgAlt: "Book cover for Pawn of Prophecy",
+    ages: "12-16",
+    genre: "Fantasy",
+    stars: "⭐⭐⭐⭐⭐"
+  }
 ];
